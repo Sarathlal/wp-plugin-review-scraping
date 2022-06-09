4d5fafec4f1edd051da06b79bbedd594b78cd3ae
Sarathlal <hello@sarathlal.com>
2022-06-09 16:00:06 +0530

    csv file saving compleeted

--- script.py
+++ script.py
@@ -3,20 +3,23 @@
 #Here is the code snippet that useful to scrap all review content of a plugin. I think, the code is self explantory.
 #
 from bs4 import BeautifulSoup
+from datetime import date
+from os.path import exists
 import requests
 import re
-from datetime import date
+import csv
 
 # https://wordpress.org/plugins/vaultpress/
 #https://wordpress.org/plugins/woocommerce-shipstation-integration/
 
-plugin_slug = "woocommerce-shipstation-integration"
+plugin_slug = "vaultpress"
 
 headers={'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15'}
 
 def main():
     for y in range(1, 6):
-        print(y)
+        print("---------------------")
+        print("Collecting " + str(y) + " star reviews")
         first_page_url="https://wordpress.org/support/plugin/" + plugin_slug + "/reviews?filter=" + str(y)
         soup = prepare_soup(first_page_url)
 
@@ -24,22 +27,25 @@ def main():
 
         if page_nums:
             # For the reviews in first page
+            print("Collecting reviews in page 1")
             links = prepare_review_links(soup)
             reviews = read_reviews(links)
-            print(reviews)
+            save_reviews(reviews, y)
 
             max_page_num = max(page_nums)
             for x in range(2, max_page_num + 1):
+                print("Collecting reviews in page " + str(x))
                 paginated_page_url = "https://wordpress.org/support/plugin/" + plugin_slug + "/reviews/page/" + str(x) + "/?filter=" + str(y)
                 soup = prepare_soup(paginated_page_url)
 
                 links = prepare_review_links(soup)
                 reviews = read_reviews(links)
-                print(reviews)
+                save_reviews(reviews, y)
+                
         else:
+            print("Collecting reviews in page 1")
             links = prepare_review_links(soup)
             reviews = read_reviews(links)
-            print(reviews)
             save_reviews(reviews, y)
 
 def prepare_soup(url):
@@ -50,12 +56,13 @@ def prepare_soup(url):
 def prepare_page_nums(soup):
     pagination_links = soup.find('div',attrs={'class':'bbp-pagination-links'})
     page_nums = []
-    for pagination_link in pagination_links:
-        page_num = pagination_link.text.strip()
-        if page_num:
-            matched=re.search("[^\d]",page_num)
-            if matched==None:
-                page_nums.append(int(page_num))
+    if pagination_links is not None:
+        for pagination_link in pagination_links:
+            page_num = pagination_link.text.strip()
+            if page_num:
+                matched=re.search("[^\d]",page_num)
+                if matched==None:
+                    page_nums.append(int(page_num))
     return page_nums
 
 def prepare_review_links(soup):
@@ -82,11 +89,17 @@ def read_reviews(links):
     return reviews
 
 def save_reviews(reviews, star):
-    print('save_reviews')
-    print(date.today())
-    print(reviews)
-    print(star)
-    
-    
+    file_name = str(date.today()) + "_" + plugin_slug + "_" + str(star) + "_star.csv"
+    if not exists(file_name):
+        fp = open(file_name, 'x')
+        fp.close()
+        print("new file created - " + file_name)
+
+    # opening the csv file in 'w+' mode
+    file = open(file_name, 'a', newline ='')
+
+    with file:   
+        write = csv.writer(file)
+        write.writerows(reviews)
 
 main()
